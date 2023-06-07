47722e31518092556a7f87c1a4de20cb4264ab7c
blacktooth <facuurdev@gmail.com>
2023-06-07 00:47:47 -0300

    modified:   agendaDjango/settings.py

--- agendaDjango/settings.py
+++ agendaDjango/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'django-insecure-9vdb*6*h%7*@3z_yz8xrzf(pxk$kjhgtd&3y2^uzra%4y&5shn'
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -80,9 +80,9 @@ WSGI_APPLICATION = 'agendaDjango.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": 'agenda',
-        "USER": 'postgres',
-        "PASSWORD": '7567white',
+        "NAME": config("DB_NAME"),
+        "USER": config("USER"),
+        "PASSWORD": config("PASSWORD"),
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
